f7d0f082b13322643c893b541a8aeef96daa802e
nazeemnato <nazeemnob18@gmail.com>
2021-01-01 21:26:00 +0530

    issue fixed

--- lib/src/remove_emoji_base.dart
+++ lib/src/remove_emoji_base.dart
@@ -3,10 +3,10 @@ import 'package:remove_emoji/src/util/emoji.dart';
 /// Accurately replace/remove emojis in text strings
 class RemoveEmoji {
   /// Accurately replace/remove emojis in text strings
-  /// Returns a copy of string with any emoji characters removed.
+  /// Returns a copy of string  without emoji characters/
   ///```
-  /// var rem = RemoveEmoji();
-  /// print(rem.removemoji('🏳️🏴🏴‍☠️🏁hi🚩🏳️‍🌈🏳️‍⚧🇺🇳'));
+  /// var remove = RemoveEmoji();
+  /// print(remove.removemoji('🏳️🏴🏴‍☠️🏁hi🚩🏳️‍🌈🏳️‍⚧🇺🇳'));
   ///
   /// //or
   ///
@@ -14,6 +14,5 @@ class RemoveEmoji {
   ///```
   String removemoji(String text, [String word = '']) => text
       .replaceAll(RegExp(r), word)
-      .trim()
-      .replaceAll(RegExp('[^\x00-\x7F]'), '');
+      .trim();
 }
