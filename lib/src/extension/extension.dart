a0a0343237cbb08f5bbacb926b4672da101ddf1a
nazeemnato <nazeemnob18@gmail.com>
2021-01-01 21:25:41 +0530

    typo fixed

--- lib/src/extension/extension.dart
+++ lib/src/extension/extension.dart
@@ -3,6 +3,6 @@ import 'package:remove_emoji/remove_emoji.dart';
 /// Accurately replace/remove emojis in text strings extension
 extension RemoveEmojiExtension on String {
   /// Accurately replace/remove emojis in text strings
-  /// Returns a copy of string with any emoji characters removed.
+  /// Returns a copy of string without emoji characters.
   String get removemoji => RemoveEmoji().removemoji(this);
 }
